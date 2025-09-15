910b0f74e5ffd27af70ed8c3915c0dbaeb667f41
kisshuasheng <2145738@qq.com>
2025-09-15 15:22:23 +0800

    1

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://103.217.184.186:21114".to_owned()
 }
 
 #[inline]
